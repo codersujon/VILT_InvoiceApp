724a1a80a855fa3598865c506d525ee0d3cf3524
unknown <csesujon155@gmail.com>
2024-09-25 19:44:30 -0700

    Invoice List Show

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Invoice;
 use Illuminate\Http\Request;
 
 class InvoiceController extends Controller
 {
-    //
+    /**
+     * get all invoice
+     */
+    public function get_all_invoice(){
+        $invoices = Invoice::with('customer')->orderBy('id', "DESC")->get();
+        return response()->json([
+            'invoices' => $invoices
+        ], 200);
+    }
 }
